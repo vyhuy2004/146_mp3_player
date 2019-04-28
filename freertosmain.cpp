0e6987d26cc12b4521a3a132e0db5f98a806cd52
Huy_Nguyen_97 <47517539+vyhuy2004@users.noreply.github.com>
2019-04-28 12:37:13 -0700

    Update freertosmain.cpp

--- freertosmain.cpp
+++ freertosmain.cpp
@@ -1,75 +1,37 @@
-/*
- *     SocialLedge.com - Copyright (C) 2013
- *
- *     This file is part of free software framework for embedded processors.
- *     You can use it and/or distribute it as long as this copyright header
- *     remains unmodified.  The code is free for personal use and requires
- *     permission to use in a commercial product.
- *
- *      THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- *      OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- *      MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- *      I SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
- *      CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- *
- *     You can reach the author of this software at :
- *          p r e e t . w i k i @ g m a i l . c o m
- */
-
-/**
- * @file
- * @brief This is the application entry point.
- *          FreeRTOS and stdio printf is pre-configured to use uart0_min.h before main() enters.
- *          @see L0_LowLevel/lpc_sys.h if you wish to override printf/scanf functions.
- *
- */
 #include "tasks.hpp"
 #include "examples/examples.hpp"
 #include "ff.h"
 #include "string.h"
 #include "stdio.h"
 
-/**
- * The main() creates tasks or "threads".  See the documentation of scheduler_task class at scheduler_task.hpp
- * for details.  There is a very simple example towards the beginning of this class's declaration.
- *
- * @warning SPI #1 bus usage notes (interfaced to SD & Flash):
- *      - You can read/write files from multiple tasks because it automatically goes through SPI semaphore.
- *      - If you are going to use the SPI Bus in a FreeRTOS task, you need to use the API at L4_IO/fat/spi_sem.h
- *
- * @warning SPI #0 usage notes (Nordic wireless)
- *      - This bus is more tricky to use because if FreeRTOS is not running, the RIT interrupt may use the bus.
- *      - If FreeRTOS is running, then wireless task may use it.
- *        In either case, you should avoid using this bus or interfacing to external components because
- *        there is no semaphore configured for this bus and it should be used exclusively by nordic wireless.
- */
-//CMD_HANDLER_FUNC(taskTest)
-//{
-//    DIR Dir;
-//    FILINFO Finfo;
-//    FRESULT returnCode = FR_OK;
-//
-//    static unsigned int numFiles = 0;       //Count number of file
-//    const char *dirPath = "1:";             //
-//    if (FR_OK != (returnCode = f_opendir(&Dir, dirPath))) {
-//        output.printf("Invalid directory: |%s| (Error %i)\n", dirPath, returnCode);
-//        return true;
-//    }
-//    for (;;)
-//    {
-//        returnCode = f_readdir(&Dir, &Finfo);
-//        if ( (FR_OK != returnCode) || !Finfo.fname[0]) {
-//            break;
-//        }
-//        //getting songname
-//        char* songname= Finfo.fname;
-//        numFiles++;
-//        //Doe something with name, send it maybe?
-//        output.printf("%s \n",songname);
-//    }
-//
-//    return true;
-//}
+
+CMD_HANDLER_FUNC(taskTest)
+{
+    DIR Dir;
+    FILINFO Finfo;
+    FRESULT returnCode = FR_OK;
+
+    static unsigned int numFiles = 0;       //Count number of file
+    const char *dirPath = "1:";             //
+    if (FR_OK != (returnCode = f_opendir(&Dir, dirPath))) {
+        output.printf("Invalid directory: |%s| (Error %i)\n", dirPath, returnCode);
+        return true;
+   }
+    for (;;)
+    {
+        returnCode = f_readdir(&Dir, &Finfo);
+        if ( (FR_OK != returnCode) || !Finfo.fname[0]) {
+            break;
+        }
+        //getting songname
+        char* songname= Finfo.fname;
+        numFiles++;
+        //Doe something with name, send it maybe?
+        output.printf("%s \n",songname);
+    }
+
+    return true;
+}
 
 
 int main(void)
